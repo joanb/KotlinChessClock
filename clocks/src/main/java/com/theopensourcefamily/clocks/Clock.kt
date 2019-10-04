dbd2f06c505a12e7a47537bf10a6c73f883547fa
joan <joanbarrosogarrido@gmail.com>
2019-10-04 21:15:39 +0200

    add blacks and whites clock ticking every second, first trial. Clocks will be deleted and refactored

--- clocks/src/main/java/com/theopensourcefamily/clocks/Clock.kt
+++ clocks/src/main/java/com/theopensourcefamily/clocks/Clock.kt
@@ -1,4 +1,27 @@
 package com.theopensourcefamily.clocks
 
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
+
 class Clock {
+
+  companion object {
+
+    var whites = true
+
+    fun getWhites() = Observable
+      .interval(1, TimeUnit.MILLISECONDS)
+      .filter { whites }
+      .filter { it.toLong() % 1000 == 0L }
+      .doOnNext { print("WHITES: $it") }
+
+    fun getBlacks() = Observable
+      .interval(1, TimeUnit.MILLISECONDS)
+      .filter { !whites }
+      .filter { it.toLong() % 1000 == 0L }
+      .doOnNext { print("BLACKS: $it") }
+  }
+
+  fun pause() {}
+  fun start() {}
 }
