3e212edf85296c0e3f8b47a0b7d1acb9087f0eeb
joan <joanbarrosogarrido@gmail.com>
2019-10-08 21:46:14 +0200

    add GameView

--- app/src/main/java/com/theopensourcefamily/chessclock/ClocksActivity.kt
+++ app/src/main/java/com/theopensourcefamily/chessclock/ClocksActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class ClocksActivity : AppCompatActivity() {
+class ClocksActivity : AppCompatActivity(), GameView {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 
-    fun render() {
-        // Just to put something in the clocks right now
-        whiteClock.text = "5:00"
-        blackClock.text = "5:00"
-    }
+  fun render() {
+    // Just to put something in the clocks right now
+    whiteClock.text = "5:00"
+    blackClock.text = "5:00"
+  }
 }
---
+++ app/src/main/java/com/theopensourcefamily/chessclock/GameView.kt
@@ -0,0 +1,3 @@
+package com.theopensourcefamily.chessclock
+
+interface GameView
