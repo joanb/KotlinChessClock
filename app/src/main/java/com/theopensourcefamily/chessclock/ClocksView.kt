0d297d2870cce6319d06729ff2f8122e15e32124
Joan <joanbarrosogarrido@gmail.com>
2019-10-10 15:48:35 +0200

    emit user clock interactions

--- app/src/main/java/com/theopensourcefamily/chessclock/ClocksView.kt
+++ app/src/main/java/com/theopensourcefamily/chessclock/ClocksView.kt
@@ -1,6 +1,10 @@
 package com.theopensourcefamily.chessclock
 
+import io.reactivex.Observable
+
 interface ClocksView {
+
+  val userInteractions: Observable<Interaction>
 
   sealed class Interaction {
     object BlackPressed: Interaction()
