10b6a68f5092af6c2e52ada72055e15989aadddf
Joan <joanbarrosogarrido@gmail.com>
2019-10-10 16:10:48 +0200

    subscribe to user interactions

--- app/src/main/java/com/theopensourcefamily/chessclock/ClocksPresenter.kt
+++ app/src/main/java/com/theopensourcefamily/chessclock/ClocksPresenter.kt
@@ -1,10 +1,24 @@
 package com.theopensourcefamily.chessclock
 
+import io.reactivex.disposables.CompositeDisposable
+
 class ClocksPresenter {
 
   private lateinit var view: ClocksView
+  private val disposables = CompositeDisposable()
 
   fun bindView(view: ClocksView) {
     this.view = view
+    disposables.add(
+      view.userInteractions
+        .subscribe(
+          { /* TODO */ },
+          { throw Exception() }
+        )
+    )
+  }
+
+  fun unbind() {
+    disposables.dispose()
   }
 }
