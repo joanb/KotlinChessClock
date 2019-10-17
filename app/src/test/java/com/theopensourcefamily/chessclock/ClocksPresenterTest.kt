9b323db3ba0289513aebdd481488904a48c22aac
joan <joanbarrosogarrido@gmail.com>
2019-10-17 20:54:49 +0200

    fix test

--- app/src/test/java/com/theopensourcefamily/chessclock/ClocksPresenterTest.kt
+++ app/src/test/java/com/theopensourcefamily/chessclock/ClocksPresenterTest.kt
@@ -1,6 +1,7 @@
 package com.theopensourcefamily.chessclock
 
 import com.jakewharton.rxrelay2.PublishRelay
+import com.jakewharton.rxrelay2.Relay
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
@@ -9,7 +10,7 @@ import org.junit.After
 import org.junit.Test
 
 class ClocksPresenterTest {
-  private val interactions = PublishRelay.create<ClocksView.Interaction>()
+  private val interactions: Relay<ClocksView.Interaction> = PublishRelay.create<ClocksView.Interaction>()
   private val view: ClocksView = mock {
     on { userInteractions } doReturn (interactions)
   }
