a1489702d937b4fa0667f8f975233bea42d635b5
Victor Vicari <victor.a.vicari@gmail.com>
2019-12-18 10:14:00 -0300

    Update MyApplication.kt

--- app/src/main/java/com/theopensourcefamily/chessclock/MyApplication.kt
+++ app/src/main/java/com/theopensourcefamily/chessclock/MyApplication.kt
@@ -12,9 +12,7 @@ class MyApplication : Application() {
     super.onCreate()
 
     startKoin {
-
       androidContext(this@MyApplication)
-
       modules(listOf(presenter, schedulers))
     }
   }
